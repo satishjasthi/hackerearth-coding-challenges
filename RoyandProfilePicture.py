d07814841670336500d720bc5c779247f60abf3d
SATISH J <satish27may@gmail.com>
2017-02-01 21:24:57 +0530

    Add python3 path at the beginning of program
    #!/usr/bin/env python3

--- RoyandProfilePicture.py
+++ RoyandProfilePicture.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 '''Description:
 Roy wants to change his profile picture on Facebook. Now Facebook has some restriction over the dimension of picture that we can upload.
 Minimum dimension of the picture can be L x L, where L is the length of the side of square.
